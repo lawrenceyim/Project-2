489a4e7b61f8a2e3526dfef37e7dfd8cdc3f17a8
lawrenceyim <lawrence.yim.010@gmail.com>
2023-07-22 01:42:42 -0400

    Update

--- Assets/Script/PlayerData.cs
+++ Assets/Script/PlayerData.cs
@@ -7,28 +7,23 @@ public class PlayerData : MonoBehaviour
 {
     [SerializeField] private int money;
     [SerializeField] private int experience;
-    private MoneyDisplay moneyDisplay;
 
     private void Start() {
         money = 0;
         experience = 0;
-        moneyDisplay = GameObject.FindGameObjectWithTag("Money Indicator").GetComponent<MoneyDisplay>();
     }
 
     public void TakeKillReward(int experience, int money) {
         this.experience += experience;
         this.money += money;
-        moneyDisplay.SetMoney(this.money);
     }
 
     public void IncreaseMoney(int amount) {
         money += amount;
-        moneyDisplay.SetMoney(money);
     }
 
     public void DecreaseMoney(int amount) {
         money -= amount;
-        moneyDisplay.SetMoney(money);
     }
 
     public bool CanAfford(int cost) {
